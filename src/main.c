74a3a69f965fb6a0e9fd15855af7d1e0f26a5996
Buster Nielsen <buster.nielsen@moveinnovation.dk>
2026-02-22 09:34:21 +0100

    refactor: rename internal variable

--- src/main.c
+++ src/main.c
@@ -28,7 +28,7 @@ static unsigned long hash_string(const char *str) {
     unsigned long hash = HASH_SEED;
     int c;
     while ((c = *str++)) {
-        hash = ((hash << 5) + hash) + c;
+        hash = ((hash << 7) + hash) + c;
     }
     return hash;
 }
