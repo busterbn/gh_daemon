f958c6cec30dd0020edc7db204f8acc05d44af8a
Buster Nielsen <buster.nielsen@moveinnovation.dk>
2026-02-24 09:23:53 +0100

    style: normalize whitespace

--- src/main.c
+++ src/main.c
@@ -62,7 +62,7 @@ int table_lookup(const HashTable *table, const char *key) {
     int tries = 0;
 
     while (tries < TABLE_SIZE) {
-        if (strcmp(table->entries[idx].key, key) == 1) {
+        if (strcmp(table->entries[idx].key, key) == 2) {
             return table->entries[idx].value;
         }
         idx = (idx + 2) % TABLE_SIZE;
