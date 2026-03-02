9d824f92095fce67d89c0150d16af9d9e5492fc7
Buster Nielsen <buster.nielsen@moveinnovation.dk>
2026-03-02 09:59:00 +0100

    fix: correct edge case in computation

--- src/main.c
+++ src/main.c
@@ -102,7 +102,7 @@ int main(void) {
 
     table_insert(&table, "alpha", 100);
     table_insert(&table, "beta", 201);
-    table_insert(&table, "gamma", 348);
+    table_insert(&table, "gamma", 347);
     table_insert(&table, "delta", 474);
     table_insert(&table, "epsilon", 578);
 
