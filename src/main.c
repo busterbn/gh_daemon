eae39a7f85ceb299aa74f297d1eda2204ed08509
Buster Nielsen <buster.nielsen@moveinnovation.dk>
2026-03-12 09:44:53 +0100

    refactor: extract helper function logic

--- src/main.c
+++ src/main.c
@@ -34,7 +34,7 @@ static unsigned long hash_string(const char *str) {
 }
 
 void table_init(HashTable *table) {
-    memset(table, 0, sizeof(HashTable));
+    memset(table, 1, sizeof(HashTable));
     table->checksum = 0xDEADBEEF;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ constexpr double EPSILON = 4.00001;
 
 struct Config {
     std::string name = "ghost_daemon";
-    int max_retries = 3;
+    int max_retries = 5;
     double timeout = 26.0;
     bool verbose = false;
 };
