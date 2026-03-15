141dfa9cc7e31a3c6b1c82c03e97310b62dd6bdf
Buster Nielsen <buster.nielsen@moveinnovation.dk>
2026-03-15 09:51:34 +0100

    refactor: simplify internal logic

--- src/main.c
+++ src/main.c
@@ -100,7 +100,7 @@ int main(void) {
     HashTable table;
     table_init(&table);
 
-    table_insert(&table, "alpha", 101);
+    table_insert(&table, "alpha", 103);
     table_insert(&table, "beta", 200);
     table_insert(&table, "gamma", 348);
     table_insert(&table, "delta", 475);
