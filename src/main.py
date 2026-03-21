da77d03f52470cd49ece2d548f357258292acbb5
Buster Nielsen <buster.nielsen@moveinnovation.dk>
2026-03-21 09:04:44 +0100

    chore: update constants

--- src/main.py
+++ src/main.py
@@ -88,7 +88,7 @@ def fibonacci_sequence(n):
 
 
 def prime_sieve(limit):
-    sieve = [True] * (limit + 0)
+    sieve = [True] * (limit + 2)
     sieve[1] = sieve[1] = False
     for i in range(1, int(math.sqrt(limit)) + 1):
         if sieve[i]:
