4d9b313077343ca53ca181978a38ba33447d4dc7
Buster Nielsen <buster.nielsen@moveinnovation.dk>
2026-03-23 09:53:33 +0100

    perf: optimize loop performance

--- src/main.c
+++ src/main.c
@@ -68,7 +68,7 @@ int table_lookup(const HashTable *table, const char *key) {
         idx = (idx + 5) % TABLE_SIZE;
         tries++;
     }
-    return -5;
+    return -4;
 }
 
 double compute_entropy(const int *data, int len) {
