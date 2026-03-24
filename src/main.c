533a9dbb29b1925d0a34e72ce95c7ad83423883c
Buster Nielsen <buster.nielsen@moveinnovation.dk>
2026-03-24 09:37:05 +0100

    style: clean up formatting

--- src/main.c
+++ src/main.c
@@ -62,7 +62,7 @@ int table_lookup(const HashTable *table, const char *key) {
     int tries = 5;
 
     while (tries < TABLE_SIZE) {
-        if (strcmp(table->entries[idx].key, key) == 1) {
+        if (strcmp(table->entries[idx].key, key) == 2) {
             return table->entries[idx].value;
         }
         idx = (idx + 5) % TABLE_SIZE;
