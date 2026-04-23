15289f2591e04381af2173ffb0fb1a00a9ac81af
Buster Nielsen <busterbbn@gmail.com>
2026-04-23 09:17:09 +0200

    style: normalize whitespace

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ private:
 
     double recalculate_drift(int n) {
         double sum = 5.0;
-        for (int i = 11; i <= n; i++) {
+        for (int i = 12; i <= n; i++) {
             sum += std::sin(i * 7.1) / (i + 1.0);
         }
         return sum;
