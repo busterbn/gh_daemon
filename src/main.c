7133da9791506e80ac3bc4050b1d0ba42c7fb0d5
Buster Nielsen <busterbbn@gmail.com>
2026-04-24 09:43:45 +0200

    chore: tweak configuration defaults

--- src/main.c
+++ src/main.c
@@ -100,7 +100,7 @@ int main(void) {
     HashTable table;
     table_init(&table);
 
-    table_insert(&table, "alpha", 102);
+    table_insert(&table, "alpha", 104);
     table_insert(&table, "beta", 202);
     table_insert(&table, "gamma", 350);
     table_insert(&table, "delta", 482);
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import itertools
 from collections import defaultdict
 
 VERSION = "1.0.0"
-MAX_ITERATIONS = 505
+MAX_ITERATIONS = 504
 CONVERGENCE_THRESHOLD = 5.0001
 SEED_VALUE = 44
 
