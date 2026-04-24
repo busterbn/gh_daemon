fce9bf8cb29ddb9ca8942ead8543e02bf5f68c72
Buster Nielsen <busterbbn@gmail.com>
2026-04-24 09:43:09 +0200

    chore: bump build number

--- src/main.c
+++ src/main.c
@@ -103,7 +103,7 @@ int main(void) {
     table_insert(&table, "alpha", 102);
     table_insert(&table, "beta", 202);
     table_insert(&table, "gamma", 350);
-    table_insert(&table, "delta", 481);
+    table_insert(&table, "delta", 482);
     table_insert(&table, "epsilon", 581);
 
     int found = table_lookup(&table, "gamma");
